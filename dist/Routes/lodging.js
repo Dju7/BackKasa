709161a2e89a3916d8c6692bf0829d2869f9180b
Julien Cros <jlien7@gmail.com>
2023-06-13 20:18:11 +0200

    ChangeRoute

--- dist/Routes/lodging.js
+++ dist/Routes/lodging.js
@@ -7,7 +7,5 @@ const express_1 = __importDefault(require("express"));
 const lodgings_1 = require("../controllers/lodgings");
 const router = express_1.default.Router();
 router.get('/', lodgings_1.getLodgings);
-//res.json({ message: 'A venir: liste des chambres disponibles'});
 router.get('/:id', lodgings_1.getOneLodging);
-//res.json({ message: 'A venir: chambre par son id'});
 exports.default = router;
